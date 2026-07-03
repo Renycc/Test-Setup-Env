bea5c5dda217973d0b1f0b95d9742a1454af2458
Renycc <rehealnycc@gmail.com>
2026-07-03 11:42:04 -0400

    Created Hello World

---
+++ backend/helloworld.py
@@ -0,0 +1,11 @@
+### Create Hello World Project - Test Project.
+helloworld = "Hello, World!"
+print("We are testing out the {}".format(helloworld))
+
+a=25
+b=15
+
+if a > b:
+    print("{} is greater than {}".format(a, b))
+
+    
